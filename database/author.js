4281a34cff8efc80dfd1a4f050045837c41b5f14
Harsh Bhalala <71023768+harshbhalala-04@users.noreply.github.com>
2021-09-30 18:43:09 +0530

    Update

--- database/author.js
+++ database/author.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 
 // Create Author Schema 
 
-// This comment is only for testing
-
-// This is comment from vs code.
-
 const AuthorSchema = mongoose.Schema({
     id: Number,
     name: String,
